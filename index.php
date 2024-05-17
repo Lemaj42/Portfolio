85d6d98dc98143b81a214e1748e343dee58b0718
lemaj42 <abdelmadjidchabane89@gmail.com>
2024-05-17 13:46:53 +0200

    init

--- index.php
+++ index.php
@@ -8,15 +8,6 @@
     <link href="./style/style_index.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-    <style>
-        ul {
-            list-style: none;
-        }
-
-        ol {
-            list-style: none;
-        }
-    </style>
 </head>
 
 <body style="overflow-x: hidden;">
